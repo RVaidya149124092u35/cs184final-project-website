b0078830b3b6d9d5e362a301b463702d75132c04
atticuscull <atticus.cull@gmail.com>
2024-04-30 23:23:04 -0700

    added some text and a comparison

--- Desktop/cs184finalsite/pctm/src/Section.js
+++ Desktop/cs184finalsite/pctm/src/Section.js
@@ -5,7 +5,7 @@ const Section = ({ title, link, children }) => {
   return (
     <section style={{ borderBottom: '2px solid black', padding: '20px 0' }}>
       <h2 style={{ textAlign: 'center', fontWeight: 'bold' }}>{title}</h2>
-        <div style={{ padding: '0 10px' }}>{children}</div>
+        <div style={{ padding: '0 50px' }}>{children}</div>
     </section>
   );
 };
